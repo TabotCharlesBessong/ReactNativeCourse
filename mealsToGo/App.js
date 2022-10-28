2cdaabb40962f70f0b49e20afa6b2e7dbd1e8439
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-10-28 16:14:08 +0100

    exercise and uodating modular export and import

--- mealsToGo/App.js
+++ mealsToGo/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View , Platform , StatusBar } from 'react-native';
+
+const isAndroid = Platform.OS === 'android'
 
 export default function App() {
   return (
-    <>
-      <SafeAreaView style={{flex:1}} >
-        <View style={{padding:16,backgroundColor:'green'}} >
-          <Text>Search</Text>
-        </View>
-        <View style={{flex:1,padding:16,backgroundColor:'blue'}} >
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
-      <StatusBar style='auto' />
-    </>
-  );
+		<>
+			<SafeAreaView
+				style={{ flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0 }}
+			>
+				<View style={{ padding: 16, backgroundColor: "green" }}>
+					<Text>Search</Text>
+				</View>
+				<View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+					<Text>List</Text>
+				</View>
+			</SafeAreaView>
+			<ExpoStatusBar style="auto" />
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
