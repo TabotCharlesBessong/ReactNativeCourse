abfcaf3745a844c07298534fac19cafbd8e357a3
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-10-30 11:40:25 +0100

    fixes

--- mealsToGo/src/features/restaurants/components/RestaurantInfo.js
+++ mealsToGo/src/features/restaurants/components/RestaurantInfo.js
@@ -3,6 +3,19 @@ import React from 'react'
 import {View, Text , StyleSheet } from 'react-native'
 import images from '../../../constant/images'
 import {Card} from 'react-native-paper'
+// import styled from 'styled-components/native'
+
+// const Title = styled(Text)`
+//   padding:16px,
+//   color:red,
+//   textAlign:'center',
+//     textTransform:'capitalize'
+
+// `;
+
+// const RestaurantCard = styled(Card)`
+//   backgroundColor:white
+// `
 
 const RestaurantInfo = ({restaurant ={} }) => {
   const {
@@ -18,7 +31,8 @@ const RestaurantInfo = ({restaurant ={} }) => {
     <View>
       <Card elevation={5} style={StyleSheet.card} >
         <Card.Cover key={name} style={styles.cover} source={{uri:photos[0]}} />
-        <Text style={styles.title} > {name} </Text> 
+        <Text style={styles.title} >{name}</Text>
+        {/* <Title> {name} </Title> */}
         <Text style={styles.title} > {address} </Text>
       </Card>
     </View>
