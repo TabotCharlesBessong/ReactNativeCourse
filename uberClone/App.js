b507c6e9b43306d47a7641040c66c22c7c7ba581
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-10-31 11:25:11 +0100

    added redux packages

--- uberClone/App.js
+++ uberClone/App.js
@@ -1,41 +1,14 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View , Platform , StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components/native';
-import {RestaurantScreen} from './src/features/index'
-import { theme } from './src/infrastructure/theme';
-import {
-	useFonts as useOswold,
-	Oswald_400Regular
-} from '@expo-google-fonts/oswald'
-import {
-	useFonts as useLato,
-	Lato_400Regular
-} from '@expo-google-fonts/lato'
- 
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
-
-	const [oswaldLoaded] = useOswold({
-		Oswald_400Regular
-	})
-
-	const [latoLoaded] = useLato({
-		Lato_400Regular,
-	});
-
-	if(!oswaldLoaded || !latoLoaded) {
-		return null
-	}
-  return (
-		<>
-			<ThemeProvider theme={theme} >
-				<RestaurantScreen />
-			</ThemeProvider>
-			<ExpoStatusBar style="auto" />
-		</>
-	);
+const App = () => {
+	return (
+		<View>
+			<Text>App</Text>
+		</View>
+	)
 }
 
-const styles = StyleSheet.create({
-	
-});
+export default App
+
+const styles = StyleSheet.create({})
