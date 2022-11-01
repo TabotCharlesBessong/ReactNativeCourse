dd5e45c43d034d810e8137e6d5befac6f5de875c
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-11-01 09:16:34 +0100

    google api

--- uberClone/src/screens/HomeScreen.js
+++ uberClone/src/screens/HomeScreen.js
@@ -4,22 +4,50 @@ import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { NavOptions } from '../components'
 import images from '../constant/images'
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete'
+import {API_KEY} from '@env'
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={tw`bg-white h-full`} >
-      <View style={tw`p-5`} >
-        <Image style={{
-          width:100,
-          height:100,
-          resizeMode:'contain'
-        }} source={{
-          uri:images.uber
-        }} />
-        <NavOptions/>
-      </View>
-    </SafeAreaView>
-  )
+		<SafeAreaView style={tw`bg-white h-full`}>
+			<View style={tw`p-5`}>
+				<Image
+					style={{
+						width: 100,
+						height: 100,
+						resizeMode: "contain",
+					}}
+					source={{
+						uri: images.uber,
+					}}
+				/>
+
+				<GooglePlacesAutocomplete
+					placeholder="Where from"
+					styles={{
+						container: {
+							flex: 0,
+							marginBottom: "2rem",
+						},
+						textInput: {
+							fontSize: 20,
+						},
+					}}
+					query={{
+						key: API_KEY,
+						language: "en",
+					}}
+					requestUrl={{
+						useOnPlatform: "all",
+						url:'localhost:19006'
+					}}
+					nearbyPlacesAPI="GooglePlacesSearch"
+					debounce={500}
+				/>
+				<NavOptions />
+			</View>
+		</SafeAreaView>
+	);
 }
 
 export default HomeScreen
