8e33b57a74e0b6b48624c881339010ec43277cb0
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-10 20:30:52 +0100

    removing a lot

--- mealsToGo/src/features/index.js
+++
@@ -1,9 +0,0 @@
-
-import RestaurantScreen from './restaurants/screens/RestaurantScreen'
-import RestaurantInfo from './restaurants/components/RestaurantInfo'
-
-
-export {
-  RestaurantScreen,
-  RestaurantInfo
-}
