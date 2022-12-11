6ce8ea3383b55ea7e31fbb24ea3d0d65da8ee3e9
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-11 21:41:23 +0100

    setup

---
+++ mealsToGo/src/component/index.js
@@ -0,0 +1,17 @@
+// import Typography from './typography/Typography'
+// import Spacer from './spacer/Spacer'
+// import FadeAnimation from './animation/FadeAnimation'
+// import FavouriteComponet from './favourites/FavouriteComponent'
+// import FavouriteBar from './favourites/FavouriteBar'
+// import CompactRestaurant from './restaurant/CompactRestaurant'
+// import SafeArea from './utility/SafeArea'
+
+// export {
+//   Typography,
+//   Spacer,
+//   FadeAnimation,
+//   FavouriteComponet,
+//   FavouriteBar,
+//   CompactRestaurant,
+//   SafeArea
+// }
