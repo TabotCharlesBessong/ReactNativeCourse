66776e85267fe3131d994b3f66829f9cd8d299e4
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-12 05:46:04 +0100

    setting up eslint and prettier

--- mealsToGo/App.js
+++ mealsToGo/App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Text, View, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { Platform } from "react-native-web";
+
+const isAndroid = Platform.OS === "android";
+
+const height = StatusBar.currentHeight;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: isAndroid ? height : 0,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
   },
 });
