6c36b1ec0171b7f568b86c9887c3ab573c87c5e7
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-12 17:46:12 +0100

    added rating

--- mealsToGo/App.js
+++ mealsToGo/App.js
@@ -1,18 +1,35 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { Text, View, SafeAreaView, StyleSheet } from "react-native";
 import {RestaurantInfoCard, RestaurantScreen} from './src/features'
-// import { ThemeProvider } from "styled-components/native";
+import { ThemeProvider } from "styled-components/native";
 
-// import { theme } from "./src/infrastructure";
+import { theme } from "./src/infrastructure";
+import {
+	useFonts as useOswald,
+	Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+
+	const [oswaldLoaded] = useOswald({
+		Oswald_400Regular,
+	});
+
+	const [latoLoaded] = useLato({
+		Lato_400Regular,
+	});
+
+	if (!oswaldLoaded || !latoLoaded) {
+		return null;
+	}
   return (
 		<>
-			{/* <ThemeProvider theme={theme}>
+			<ThemeProvider theme={theme}>
 				<RestaurantScreen />
-			</ThemeProvider> */}
-      <RestaurantScreen/>
-			<RestaurantInfoCard />
+			</ThemeProvider>
+      {/* <RestaurantScreen/> */}
+			{/* <RestaurantInfoCard /> */}
 			<ExpoStatusBar style="auto" />
 		</>
 	);
--- mealsToGo/src/features/restaurants/component/RestaurantInfoCard.js
+++ mealsToGo/src/features/restaurants/component/RestaurantInfoCard.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet,View ,Image} from "react-native";
 import { Card } from "react-native-paper";
 import images from "../../../constant/images";
 import styled from "styled-components/native"
+import { Icon } from "react-native-elements";
+// import { SvgXml } from "react-native-svg";
 
 const Title = styled(Text)`
-	padding: 16px;
+	${'' /* font-family: ${(props) => props.theme.fonts.body}; */}
+	${'' /* padding: ${(props) => props.theme.space[3]}; */}
+	padding:16px
 	color: red;
+	font-family: "Oswald_400Regular";
 `;
 
 const RestaurantCard = styled(Card)`
 	background-color: white;
 `;
+
+// const Rating = styled.View`
+// 	flex-direction: row;
+// 	padding-top: ${(props) => props.theme.space[2]};
+// 	padding-bottom: ${(props) => props.theme.space[2]};
+// `;
+
+
+
+// const star = images.star
 
 const RestaurantCardCover = styled(Card.Cover)`
 	padding: 20px;
@@ -29,11 +44,24 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
 		isClosedTemporarily,
 	} = restaurant;
 
+	const ratingArray = Array.from(new Array(Math.floor(rating)));
+
 	return (
 		<RestaurantCard elevation={5}>
-      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
-    </RestaurantCard>
+			<RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+			<Title>{name}</Title>
+			{/* <Rating>
+				{ratingArray.map(() => (
+					<SvgXml xml={star} width={20} height={20} />
+				))}
+			</Rating> */}
+			<View style={styles.rating}>
+				<Icon style={styles.star} name="star" size={30} color="orange" />
+				<Icon style={styles.star} name="star" size={30} color="orange" />
+				<Icon style={styles.star} name="star" size={30} color="orange" />
+				<Icon style={styles.star} name="star" size={30} color="orange" />
+			</View>
+		</RestaurantCard>
 	);
 };
 
@@ -43,4 +71,13 @@ const styles = StyleSheet.create({
 	card: { backgroundColor: "white" },
 	cover: { padding: 20, backgroundColor: "white" },
 	title: { padding: 16 },
+	rating:{
+		display:'flex',
+		justifyContent:'center',
+		alignItems: 'center',
+		flexDirection:'row'
+	},
+	star:{
+		marginLeft:8
+	}
 });
