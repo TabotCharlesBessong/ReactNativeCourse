b939e418f3fc506ca9c55010be681f8b4d46c3b3
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-19 07:20:14 +0100

    implemented basic redux

--- UBerClone/App.js
+++ UBerClone/App.js
@@ -1,45 +1,21 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/infrastructure/theme";
-import {
-	useFonts as useOswald,
-	Oswald_400Regular,
-} from "@expo-google-fonts/oswald";
-import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-import { RestaurantContextProvider } from "./src/services/restaurant/restaurantContext";
-import { LocationContextProvider } from "./src/services/location/locationContext";
-import { Navigation } from "./src/infrastructure/navigation";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {Provider} from 'react-redux'
+import { store } from './redux/store'
 
-// import { SafeArea } from "./src/components/utility/safe-area.component";
+//1 setup redux 
 
-
-
-export default function App() {
-  
-	// loading the fonts into the app 
-	const [oswaldLoaded] = useOswald({
-		Oswald_400Regular,
-	});
-
-	const [latoLoaded] = useLato({
-		Lato_400Regular,
-	});
-
-	if (!oswaldLoaded || !latoLoaded) {
-		return null;
-	}
-  return (
-		<>
-			<ThemeProvider theme={theme}>
-				<LocationContextProvider>
-					<RestaurantContextProvider>
-						<Navigation/>
-					</RestaurantContextProvider>
-				</LocationContextProvider>
-			</ThemeProvider>
-			<ExpoStatusBar style="auto" />
-		</>
+const App = () => {
+	return (
+		<Provider store={store} >
+			<View>
+				<Text>App</Text>
+				<Text>Lets build the uber clone </Text>
+			</View>
+		</Provider>
 	);
 }
 
+export default App
 
+const styles = StyleSheet.create({})
