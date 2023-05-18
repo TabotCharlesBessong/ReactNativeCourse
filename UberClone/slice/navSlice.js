9dda311d09c4c0be8f49b64a40f51fb092a50d75
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-05-18 14:22:05 +0100

    setting up redux and api key

---
+++ UberClone/slice/navSlice.js
@@ -0,0 +1,41 @@
+
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  origin:null,
+  destination:null,
+  travelTimeInformation:null
+}
+
+export const navSlice = createSlice({
+  name:'nav',
+  initialState,
+  reducers:{
+    setOrigin:(state,action) => {
+      state.origin = action.payload
+    },
+    setDestination:(state,action) => {
+      state.destination = action.payload
+    },
+    setTimeTravelInformation:(state,action) => {
+      state.travelTimeInformation = action.payload
+    }
+  }
+})
+
+export const {setOrigin,setDestination,setTimeTravelInformation} = navSlice.actions
+
+// selectors
+export const selectOrigin = (state) => {
+  state.nav.origin
+}
+
+export const selectDestination = (state) => {
+  state.nav.destination
+}
+
+export const selectTraveltimeInformation = (state) => {
+  state.nav.travelTimeInformation
+}
+
+export default navSlice.reducer
