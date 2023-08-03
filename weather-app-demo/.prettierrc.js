895f6db8e94df147984c9f70901d9ceee8800404
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2023-08-03 11:12:23 +0100

    Delete weather-app-demo directory

--- weather-app-demo/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  singleQuote: true,
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: "none",
-  semi: false
-};
--- weather-app-demo/README.md
+++
@@ -1,2 +0,0 @@
-# weather-app-example
-Sample code for the Free Code Camp beginners course on React Native.
