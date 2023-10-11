05604c142262f5b4aca47c6d1a1caf159bd83a2a
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2023-10-11 07:49:54 +0100

    Delete mealsToGo directory

--- mealsToGo/src/component/utility/SafeAreas.js
+++
@@ -1,9 +0,0 @@
-import { StatusBar, SafeAreaView } from "react-native";
-import styled from "styled-components/native";
-
-const SafeAreas = styled(SafeAreaView)`
-	flex: 1;
-	${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
-
-export default SafeAreas;
--- mealsToGo/src/infrastructure/navigation/index.js
+++
@@ -1,7 +0,0 @@
-
-import React from "react"
-import AppNavigator from "./appNavigator"
-
-export const  Navigation = () => {
-  return <AppNavigator/> 
-}
