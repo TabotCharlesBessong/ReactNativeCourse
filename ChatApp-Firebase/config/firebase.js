4e7823859e37c187a9cb9bdbbd05493279222b3f
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-13 07:19:49 +0100

    constant setup for security purpose

---
+++ ChatApp-Firebase/config/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import Constants from "expo-constants";
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.apiKey,
+  authDomain: Constants.expoConfig.extra.authDomain,
+  projectId: Constants.expoConfig.extra.projectId,
+  storageBucket: Constants.expoConfig.extra.storageBucket,
+  messagingSenderId: Constants.expoConfig.extra.messagingSenderId,
+  appId: Constants.expoConfig.extra.apiId,
+  measurementId: Constants.expoConfig.extra.measurementId,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const database = getFirestore();
+export const auth = getAuth(app);
