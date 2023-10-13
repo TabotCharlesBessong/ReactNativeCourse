1fb2d102ed064ae44f1041cb72d4bc396c3cdbfb
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-13 08:00:59 +0100

    started working on screens

--- ChatApp-Firebase/App.js
+++ ChatApp-Firebase/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { View } from "react-native";
+import { Chat } from "./screens";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  );
+};
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ ChatApp-Firebase/screens/index.js
@@ -0,0 +1,5 @@
+
+export {default as Chat} from './Chat'
+export {default as Home} from './Home'
+export {default as Login} from './Login'
+export {default as Signup} from './Signup'
