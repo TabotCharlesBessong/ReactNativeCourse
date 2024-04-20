83ca0f56cf3c5d2f747349aa5f9224dd1d9f1ce0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-04-20 17:34:22 +0100

    completed the songs screen

--- music_app/app/(tabs)/(songs)/index.tsx
+++ music_app/app/(tabs)/(songs)/index.tsx
@@ -1,17 +1,33 @@
-import { TracksList } from "@/components"
-import { defaultStyles } from "@/styles"
-import React from "react"
-import { ScrollView, View } from "react-native"
-
+import { TracksList } from "@/components";
+import { screenPadding } from "@/constants/tokens";
+import { useNavigationSearch } from "@/hooks/useNavigationSearch";
+import { defaultStyles } from "@/styles";
+import React, { useMemo } from "react";
+import { ScrollView, View } from "react-native";
+import library from "../../../assets/data/library.json"
+import { trackTitleFilter } from "@/helpers/filter";
 
 const SongsScreen = () => {
+  const search = useNavigationSearch({
+    searchBarOptions:{
+      placeholder:"Find in songs"
+    }
+  })
+
+  const filteredSongs = useMemo(() => {
+    if(!search) return library
+    return library.filter(trackTitleFilter(search))
+  },[search])
   return (
-    <View style={defaultStyles.container} >
-      <ScrollView>
-        <TracksList scrollEnabled={false} />
+    <View style={defaultStyles.container}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={{ marginTop: 72, paddingHorizontal: screenPadding.horizontal }}
+      >
+        <TracksList tracks={filteredSongs} scrollEnabled={false} />
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default SongsScreen
+export default SongsScreen;
--- music_app/components/tracks/TracksList.tsx
+++ music_app/components/tracks/TracksList.tsx
@@ -2,14 +2,23 @@ import { FlatList, FlatListProps, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import library from "../../assets/data/library.json";
 import TrackListItem from "./TrackListItem";
+import {utilsStyles} from "../../styles"
 
-export type TrackListProps = Partial<FlatListProps<unknown>>;
+export type TrackListProps = Partial<FlatListProps<unknown>> & {
+  tracks:any[]
+}
 
+const ItemDivider = () => (
+  <View style={{...utilsStyles,marginVertical:9,marginLeft:16}} />
+)
 
-const TracksList = ({...flatListProps}:TrackListProps) => {
+const TracksList = ({tracks,...flatListProps}:TrackListProps) => {
   return (
     <FlatList
-      data={library}
+      data={tracks}
+      contentContainerStyle={{paddingTop:10,paddingBottom:120}}
+      ListFooterComponent={ItemDivider}
+      ItemSeparatorComponent={ItemDivider}
       renderItem={({ item: track }) => (
         <TrackListItem
           track={{
