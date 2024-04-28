99bdcbcf955bd168df5639f1193fd350545cad03
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-04-28 05:50:44 +0100

    done with basic configuration

--- expense_tracker/app/_layout.tsx
+++ expense_tracker/app/_layout.tsx
@@ -50,11 +50,8 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        
       </Stack>
-    </ThemeProvider>
   );
 }
