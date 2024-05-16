3123cd94034b6b0508c73f192baa57293eee4724
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-16 22:12:06 +0100

    loading the database into the app

--- budget_app/App.tsx
+++ budget_app/App.tsx
@@ -1,8 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
+
+const loadDB = async () => {
+  const dbName = "budget.db";
+  const dbAsset = require("./assets/budget.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+};
 
 export default function App() {
+  const [dbLoaded, setDbLoaded] = useState<boolean>(true)
+
+  useEffect(() => {
+    loadDB().then(() => setDbLoaded(false)).catch((e) => console.log(e))
+  },[])
+
+  if(dbLoaded){
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+        <Text>Loading....</Text>
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
