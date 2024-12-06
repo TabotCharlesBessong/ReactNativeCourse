b6112d098bf8a36091c25758df84603d896f9c48
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-06 04:17:49 +0100

    implemented file based navigation

---
+++ gadget_shop_app/src/app/_layout.tsx
@@ -0,0 +1,35 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(shop)"
+        options={{ headerShown: true, title: "Shop" }}
+      />
+      <Stack.Screen
+        name="categories"
+        options={{ headerShown: true, title: "Category" }}
+      />
+      <Stack.Screen
+        name="product"
+        options={{ headerShown: true, title: "Product" }}
+      />
+      <Stack.Screen
+        name="cart"
+        options={{
+          headerShown: true,
+          title: "Shopping Cart",
+          presentation: "modal",
+        }}
+      />
+      <Stack.Screen
+        name="auth"
+        options={{ headerShown: true, title: "Auth" }}
+      />
+      {/* <Stack.Screen name="(shop)" options={{headerShown:true,title:'Shop'}} /> */}
+      {/* <Stack.Screen name="(shop)" options={{headerShown:true,title:'Shop'}} /> */}
+    </Stack>
+  );
+}
---
+++ gadget_shop_app/src/app/auth.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const auth = () => {
+  return (
+    <View>
+      <Text>auth</Text>
+    </View>
+  )
+}
+
+export default auth
