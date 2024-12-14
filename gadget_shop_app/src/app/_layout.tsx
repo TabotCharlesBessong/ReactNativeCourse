64140b65c2798c339fe232285152ae16c1af27c1
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-15 00:53:44 +0100

    product details page

--- gadget_shop_app/src/app/_layout.tsx
+++ gadget_shop_app/src/app/_layout.tsx
@@ -1,33 +1,36 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { ToastProvider } from "react-native-toast-notifications";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(shop)"
-        options={{ headerShown: false, title: "Shop" }}
-      />
-      <Stack.Screen
-        name="categories"
-        options={{ headerShown: false, title: "Category" }}
-      />
-      <Stack.Screen
-        name="product"
-        options={{ headerShown: true, title: "Product" }}
-      />
-      <Stack.Screen
-        name="cart"
-        options={{
-          headerShown: true,
-          title: "Shopping Cart",
-          presentation: "modal",
-        }}
-      />
-      <Stack.Screen
-        name="auth"
-        options={{ headerShown: true, title: "Auth" }}
-      />
-    </Stack>
+    <ToastProvider>
+      <Stack>
+        <Stack.Screen
+          name="(shop)"
+          options={{ headerShown: false, title: "Shop" }}
+        />
+        <Stack.Screen
+          name="categories"
+          options={{ headerShown: false, title: "Category" }}
+        />
+        <Stack.Screen
+          name="product"
+          options={{ headerShown: false, title: "Product" }}
+        />
+        <Stack.Screen
+          name="cart"
+          options={{
+            headerShown: true,
+            title: "Shopping Cart",
+            presentation: "modal",
+          }}
+        />
+        <Stack.Screen
+          name="auth"
+          options={{ headerShown: true, title: "Auth" }}
+        />
+      </Stack>
+    </ToastProvider>
   );
 }
