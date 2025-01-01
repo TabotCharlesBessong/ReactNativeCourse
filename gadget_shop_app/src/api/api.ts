be14f24daf0bf9a2b4010b752ad6bbbae9e5e8ed
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-01-01 18:17:49 +0100

    product and category details page

--- gadget_shop_app/src/api/api.ts
+++ gadget_shop_app/src/api/api.ts
@@ -16,3 +16,53 @@ export const getProductsAndCategories = () => {
     },
   });
 };
+
+
+export const getProduct = (slug: string) => {
+  return useQuery({
+    queryKey: ["product", slug],
+    queryFn: async () => {
+      const { data, error } = await supabase
+        .from("product")
+        .select("*")
+        .eq("slug", slug)
+        .single();
+
+      if (error || !data) {
+        throw new Error(
+          "An error occurred while fetching data: " + error?.message
+        );
+      }
+
+      return data;
+    },
+  });
+};
+
+export const getCategoryAndProducts = (categorySlug: string) => {
+  return useQuery({
+    queryKey: ["categoryAndProducts", categorySlug],
+    queryFn: async () => {
+      const { data: category, error: categoryError } = await supabase
+        .from("category")
+        .select("*")
+        .eq("slug", categorySlug)
+        .single();
+
+      if (categoryError || !category) {
+        throw new Error("An error occurred while fetching category data");
+      }
+
+      const { data: products, error: productsError } = await supabase
+        .from("product")
+        .select("*")
+        .eq("category", category.id);
+
+      if (productsError) {
+        throw new Error("An error occurred while fetching products data");
+      }
+
+      return { category, products };
+    },
+  });
+};
