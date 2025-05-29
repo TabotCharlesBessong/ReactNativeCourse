e39eaae7cadd4a5132b7db722b50b0bdaf492a8b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-29 08:47:37 +0100

    implemented authentication

--- habit_tracking/app/_layout.tsx
+++ habit_tracking/app/_layout.tsx
@@ -1,9 +1,47 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { Stack, useRouter } from "expo-router";
+import * as React from "react";
+import { ActivityIndicator, View } from "react-native";
+import { AuthProvider, useAuth } from "../lib/auth-context";
+
+const RouteGuard = ({ children }: { children: React.ReactNode }) => {
+  const { user, isLoadingUser } = useAuth();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        if (!isLoadingUser && !user) {
+          router.replace("/auth");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    
+    checkAuth();
+  }, [user, isLoadingUser]);
+
+  if (isLoading || isLoadingUser) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return <>{children}</>;
+};
 
 export default function RootLayout() {
-  return (<Stack >
-    {/* <Stack.Screen name="index" options={{ headerShown:true }} /> */}
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-  </Stack>);
+  return (
+    <AuthProvider>
+      <RouteGuard>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="auth" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </RouteGuard>
+    </AuthProvider>
+  );
 }
---
+++ habit_tracking/app/auth.tsx
@@ -0,0 +1,126 @@
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
+import * as React from "react";
+import { useAuth } from "@/lib/auth-context";
+
+export default function AuthScreen() {
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>("");
+
+  const theme = useTheme();
+  const router = useRouter();
+
+  const { signIn, signUp } = useAuth();
+
+  const handleAuth = async () => {
+    if (!email || !password) {
+      setError("Please fill in all fields.");
+      return;
+    }
+
+    if (password.length < 6) {
+      setError("Passwords must be at least 6 characters long.");
+      return;
+    }
+
+    setError(null);
+
+    if (isSignUp) {
+      const error = await signUp(email, password);
+      if (error) {
+        setError(error);
+        return;
+      }
+    } else {
+      const error = await signIn(email, password);
+      if (error) {
+        setError(error);
+        return;
+      }
+
+      router.replace("/");
+    }
+  };
+
+  const handleSwitchMode = () => {
+    setIsSignUp((prev) => !prev);
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <View style={styles.content}>
+        <Text style={styles.title} variant="headlineMedium">
+          {" "}
+          {isSignUp ? "Create Account" : "Welcome Back"}
+        </Text>
+
+        <TextInput
+          label="Email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          placeholder="example@gmail.com"
+          mode="outlined"
+          style={styles.input}
+          onChangeText={setEmail}
+        />
+
+        <TextInput
+          label="Password"
+          autoCapitalize="none"
+          mode="outlined"
+          secureTextEntry
+          style={styles.input}
+          onChangeText={setPassword}
+        />
+
+        {error && <Text style={{ color: theme.colors.error }}> {error}</Text>}
+
+        <Button mode="contained" style={styles.button} onPress={handleAuth}>
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </Button>
+
+        <Button
+          mode="text"
+          onPress={handleSwitchMode}
+          style={styles.switchModeButton}
+        >
+          {isSignUp
+            ? "Already have an account? Sign In"
+            : "Don't have an account? Sign Up"}
+        </Button>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 8,
+  },
+  switchModeButton: {
+    marginTop: 16,
+  },
+});
