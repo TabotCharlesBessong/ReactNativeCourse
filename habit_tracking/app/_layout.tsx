36b32bcfc18f4f24e0887e15a7b0f635a562f55c
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-29 17:59:33 +0100

    completed with the adding habit and streak screen and also fix some issues

--- habit_tracking/app/_layout.tsx
+++ habit_tracking/app/_layout.tsx
@@ -1,10 +1,11 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack, useRouter, useSegments } from "expo-router";
 import * as React from "react";
 import { ActivityIndicator, View } from "react-native";
-import { AuthProvider, useAuth } from "../lib/auth-context";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { PaperProvider } from "react-native-paper";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AuthProvider, useAuth } from "../lib/auth-context";
 
 const queryClient = new QueryClient();
 
@@ -41,20 +42,21 @@ const RouteGuard = ({ children }: { children: React.ReactNode }) => {
 
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient} >
-
-    <AuthProvider>
-      <PaperProvider>
-        <SafeAreaProvider>
-          <RouteGuard>
-            <Stack screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="auth" />
-              <Stack.Screen name="(tabs)" />
-            </Stack>
-          </RouteGuard>
-        </SafeAreaProvider>
-      </PaperProvider>
-    </AuthProvider>
-    </QueryClientProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <QueryClientProvider client={queryClient} >
+        <AuthProvider>
+          <PaperProvider>
+            <SafeAreaProvider>
+              <RouteGuard>
+                <Stack screenOptions={{ headerShown: false }}>
+                  <Stack.Screen name="auth" />
+                  <Stack.Screen name="(tabs)" />
+                </Stack>
+              </RouteGuard>
+            </SafeAreaProvider>
+          </PaperProvider>
+        </AuthProvider>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   );
 }
