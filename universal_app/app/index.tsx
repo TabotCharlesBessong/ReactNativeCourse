e6b89109dd3b07b2ea25ed9d2e33d9e4f8ba1e21
TabotCharlesBessong <ebezebeatrice@gmail.com>
2026-06-06 05:07:41 +0100

    basic introduction to navigation

--- universal_app/app/index.tsx
+++ universal_app/app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
@@ -6,6 +7,7 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Welcome to the home screen</Text>
+      <Link href="/about" style={styles.button}>About</Link>
     </View>
   );
 }
@@ -21,5 +23,16 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 20,
     fontWeight: "bold",
+  },
+  button:{
+    backgroundColor: "#fff",
+    color: "#223",
+    padding: 10,
+    borderRadius: 5,
+    margin: 10,
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    // textDecorationLine: "underline",
   }
 })
