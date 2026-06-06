9885504956abb0a4de968baa9c1a0d32555115e8
TabotCharlesBessong <ebezebeatrice@gmail.com>
2026-06-06 05:26:30 +0100

    navigation settings

--- universal_app/app/+not-found.tsx
+++ universal_app/app/+not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: "Oops! Not Found" }} />
       <View style={styles.container}>
-        <Link href="/" style={styles.button}>
+        <Link href="/(tabs)/index" style={styles.button}>
           Go back to Home screen!
         </Link>
       </View>
