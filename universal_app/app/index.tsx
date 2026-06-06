ea62908eb24bb335719c62ec6809dc75543606f6
TabotCharlesBessong <ebezebeatrice@gmail.com>
2026-06-06 04:52:38 +0100

    introduction to stylesheet

--- universal_app/app/index.tsx
+++ universal_app/app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Welcome to the home screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#223",
+  },
+  text:{
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  }
+})
